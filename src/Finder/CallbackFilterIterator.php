e57b2989b19a6f91995ca54c942eafc2b4e17377
David Grudl <david@grudl.com>
2010-09-14 22:54:58 +0200

    born Finder

---
+++ src/Finder/CallbackFilterIterator.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework.
+ *
+ * Copyright (c) 2004, 2010 David Grudl (http://davidgrudl.com)
+ *
+ * This source file is subject to the "Nette license", and/or
+ * GPL license. For more information please see http://nette.org
+ */
+
+namespace Nette;
+
+use Nette;
+
+
+
+/**
+ * Callback iterator filter.
+ *
+ * @author     David Grudl
+ */
+class CallbackFilterIterator extends \FilterIterator
+{
+	/** @var callback */
+	private $callback;
+
+
+	/**
+	 * Constructs a filter around another iterator.
+	 * @param
+	 * @param  callback
+	 */
+	function __construct(\Iterator $iterator, $callback)
+	{
+		parent::__construct($iterator);
+		$this->callback = $callback;
+	}
+
+
+
+	function accept()
+	{
+		return call_user_func($this->callback, $this);
+	}
+
+}
